f4df703ed9149a370c7cc53db1770b48d02b79f0
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-02-11 12:07:55 +0100

    implement redux

--- Program.fs
+++ Program.fs
@@ -1,4 +1,8 @@
-﻿[<EntryPoint>]
+﻿let store (init: 'State) (reducer: 'State -> 'Action -> 'State) =
+    let inputEvent = Event<'Action>()
+    Observable.scan reducer init inputEvent.Publish, inputEvent.Trigger
+
+[<EntryPoint>]
 let main _ =
     printfn "Hello, world!"
     0 // return an integer exit code
